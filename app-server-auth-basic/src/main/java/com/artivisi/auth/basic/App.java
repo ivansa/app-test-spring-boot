fcfa8f129cf67dc58bac78a5881e7ffc6513f351
Ivans Ardiansyah <vaansaa@gmail.com>
2016-03-10 12:43:17 +0700

    auth basic

---
+++ app-server-auth-basic/src/main/java/com/artivisi/auth/basic/App.java
@@ -0,0 +1,25 @@
+package com.artivisi.auth.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+}
